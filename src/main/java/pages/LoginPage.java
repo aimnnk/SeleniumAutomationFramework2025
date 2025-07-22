4743c1afcc0309e70ec8c890be44ff750ad1eb72
Muhammad Aiman Bin Khalid <SDABKMUHA@slake.sssb.com>
2025-07-22 12:04:14 +0800

    Add page factory

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,40 +1,59 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import utils.Log;
 
 public class LoginPage {
 
 	private WebDriver driver;
+	
+	@FindBy(id="Email")
+	WebElement usernameTextbox;
+	
+	@FindBy(id="Password")
+	WebElement passwordTextbox;
+	
+	@FindBy(xpath="//*[@id=\\\"main\\\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button")
+	WebElement LoginButton;
 
 	// element of the page
-	private By usernameTextBox = By.id("Email");
-	private By passwordTextBox = By.id("Password");
-	private By loginButton = By.xpath("//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button");
+//	private By usernameTextBox = By.id("Email");
+//	private By passwordTextBox = By.id("Password");
+//	private By loginButton = By.xpath("//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button");
 
 	// constructor - function in the same class name
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
 	// adding action function
 	public void enterUsername(String username) {
 		Log.info("Entering username: " + username);
-		driver.findElement(usernameTextBox).clear();
-		driver.findElement(usernameTextBox).sendKeys(username);
+		
+		usernameTextbox.clear();
+		usernameTextbox.sendKeys(username);
+//		driver.findElement(usernameTextBox).clear();
+//		driver.findElement(usernameTextBox).sendKeys(username);
 	}
 
 	public void enterPassword(String password) {
 		Log.info("Entering password: *****");
-		driver.findElement(usernameTextBox).clear();
-		driver.findElement(usernameTextBox).sendKeys("");
+		
+		passwordTextbox.clear();
+		passwordTextbox.sendKeys("");
+//		driver.findElement(usernameTextBox).clear();
+//		driver.findElement(usernameTextBox).sendKeys("");
 	}
 
 	public void clickLogin() {
-		
 		Log.info("Clicking login button.");
-		driver.findElement(loginButton).click();
+		
+		LoginButton.click();
+//		driver.findElement(loginButton).click();
 	}
 }
